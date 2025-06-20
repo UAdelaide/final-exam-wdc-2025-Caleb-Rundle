c1dcc7b4a9e2898dd5e7791c5850dcbaa195e0e4
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 02:14:11 +0000

    [automatic commit] 2025-06-20 02:14:11 UTC 203.219.193.98

--- part1/app.js
+++ part1/app.js
@@ -4,10 +4,9 @@ const app = express();
 const PORT = 3000;
 
 app.listen(PORT, (error) => {
-    if(!error) {
-        console.log('Server running on port ' + PORT);
-    }
-    else {
-        console.log('Error occured, serve')
-    }
-})
+  if (!error) {
+    console.log('Server running on port ' + PORT);
+  } else {
+    console.log('Failure to start: ', error);
+  }
+});

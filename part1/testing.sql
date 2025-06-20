3216415d7b75f2de25b9ca7944c804d21cd71319
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 04:26:26 +0000

    [automatic commit] 2025-06-20 04:26:26 UTC 203.219.193.98

--- part1/testing.sql
+++ part1/testing.sql
@@ -10,5 +10,5 @@ WalkRequests.location,
 Users.username AS owner_username
 FROM WalkRequests
 INNER JOIN Dogs ON Dogs.dog_id=WalkRequests.dog_id
-INNER JOIN Users ON 
+INNER JOIN Users ON Dogs.owner_id=Users.user_id
 WHERE WalkRequests.status='open';

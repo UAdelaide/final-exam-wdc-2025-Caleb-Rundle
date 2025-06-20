2ef18692a81ef89aa6f601bd4ea39a56935f2204
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 04:20:05 +0000

    [automatic commit] 2025-06-20 04:20:05 UTC 203.219.193.98

--- part1/testing.sql
+++ part1/testing.sql
@@ -1,4 +1,4 @@
 SELECT Dogs.name AS 'dog_name', Dogs.size AS 'size', Users.username AS 'owner_username' FROM Dogs
 INNER JOIN Users ON Dogs.owner_id=Users.user_id;
 
-SELECT WalkRequests.
+SELECT WalkRequests.request_id, Dogs.name as dog_name, 

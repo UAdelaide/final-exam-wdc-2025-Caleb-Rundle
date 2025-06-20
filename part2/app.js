c3d7192eaf3ae79279ae77a26342eedf6ab413c7
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 21:59:52 +0000

    [automatic commit] 2025-06-20 21:59:52 UTC 203.219.193.98

--- part2/app.js
+++ part2/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const mysql = require('mysql');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(PORT, (error) => {
+  if (!error) {
+    console.log('Server running on port ' + PORT);
+  } else {
+    console.log('Failure to start: ', error);
+  }
+});
 
 let count = 0;
 app.use((req, res, next) => {

5a40ebdb26b0cea01d80e1b434e797d1cdd7c17c
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 02:54:42 +0000

    [automatic commit] 2025-06-20 02:54:42 UTC 203.219.193.98

--- part1/app.js
+++ part1/app.js
@@ -44,19 +44,4 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  req.pool = pool;
-  req.sqlQuery = async (query, variables) =>
-    new Promise((resolve, reject) => {
-      pool.query(query, variables, (queryError, results) => {
-        if (queryError) {
-          reject(queryError);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-  next();
-});
-
 app.use('/api', apiRouter);

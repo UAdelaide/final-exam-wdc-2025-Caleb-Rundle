8ccf651068a21d008929fc5f111b8649d51c4b5d
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 08:33:44 +0000

    [automatic commit] 2025-06-20 08:33:44 UTC 203.219.193.98

--- part1/testing.sql
+++ part1/testing.sql
@@ -1,17 +1,17 @@
-SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs
-INNER JOIN Users ON Dogs.owner_id=Users.user_id;
+-- SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username FROM Dogs
+-- INNER JOIN Users ON Dogs.owner_id=Users.user_id;
 
-SELECT
-WalkRequests.request_id,
-Dogs.name as dog_name,
-WalkRequests.requested_time,
-WalkRequests.duration_minutes,
-WalkRequests.location,
-Users.username AS owner_username
-FROM WalkRequests
-INNER JOIN Dogs ON Dogs.dog_id=WalkRequests.dog_id
-INNER JOIN Users ON Dogs.owner_id=Users.user_id
-WHERE WalkRequests.status='open';
+-- SELECT
+-- WalkRequests.request_id,
+-- Dogs.name as dog_name,
+-- WalkRequests.requested_time,
+-- WalkRequests.duration_minutes,
+-- WalkRequests.location,
+-- Users.username AS owner_username
+-- FROM WalkRequests
+-- INNER JOIN Dogs ON Dogs.dog_id=WalkRequests.dog_id
+-- INNER JOIN Users ON Dogs.owner_id=Users.user_id
+-- WHERE WalkRequests.status='open';
 
 SELECT user_id FROM Users
 WHERE role='walker';

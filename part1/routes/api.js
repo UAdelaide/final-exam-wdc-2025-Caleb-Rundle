bd126869d7fd8faaf4c7dd4e24a5a6b8fdfb5099
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 09:21:58 +0000

    [automatic commit] 2025-06-20 09:21:58 UTC 203.219.193.98

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -74,7 +74,7 @@ GROUP BY WalkApplications.walker_id, Users.username;
   } catch (queryError) {
     return res.status(500).send(queryError);
   }
-  res.send(firstResult);
+  res.send(secondResult);
 });
 
 module.exports = router;

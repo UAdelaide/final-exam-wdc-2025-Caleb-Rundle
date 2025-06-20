92c9590511ccf02764e59d01e66a646492884348
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 07:41:30 +0000

    [automatic commit] 2025-06-20 07:41:30 UTC 203.219.193.98

--- part1/testing.sql
+++ part1/testing.sql
@@ -13,20 +13,6 @@ INNER JOIN Dogs ON Dogs.dog_id=WalkRequests.dog_id
 INNER JOIN Users ON Dogs.owner_id=Users.user_id
 WHERE WalkRequests.status='open';
 
-CREATE TABLE WalkRatings (
-    rating_id INT AUTO_INCREMENT PRIMARY KEY,
-    request_id INT NOT NULL,
-    walker_id INT NOT NULL,
-    owner_id INT NOT NULL,
-    rating INT CHECK (rating BETWEEN 1 AND 5),
-    comments TEXT,
-    rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
-    FOREIGN KEY (walker_id) REFERENCES Users(user_id),
-    FOREIGN KEY (owner_id) REFERENCES Users(user_id),
-    CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
-);
-
 INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating) VALUES
 (1, 2, 1, 3),
 (2, 2, 1, 4),

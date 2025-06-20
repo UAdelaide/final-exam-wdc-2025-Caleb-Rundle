25c05c5c3c2da312fed2a3b945a8d3af45e298b0
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 01:41:02 +0000

    [automatic commit] 2025-06-20 01:41:02 UTC 203.219.193.98

--- part1/query.sql
+++ part1/query.sql
@@ -19,7 +19,7 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username='alice123'), 'Max Senior', 'large'),
 ((SELECT user_id FROM Users WHERE username='james123'), 'John Lick', 'large');
 
-INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes)
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 
 SELECT * FROM Users;
 SELECT * FROM Dogs;

44fb4fb9a298e170d739fe9247528b1a7af95933
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 08:13:59 +0000

    [automatic commit] 2025-06-20 08:13:59 UTC 203.219.193.98

--- part1/testing.sql
+++ part1/testing.sql
@@ -26,3 +26,7 @@ SELECT COUNT(*) FROM WalkApplications
 INNER JOIN WalkRequests on WalkRequests.request_id=WalkApplications.request_id
 WHERE WalkRequests.status='completed'
 GROUP BY WalkApplications.walker_id;
+
+SELECT COUNT(*), WalkRequests.status FROM WalkApplications
+INNER JOIN WalkRequests on WalkRequests.request_id=WalkApplications.request_id
+GROUP BY WalkApplications.walker_id;

7301483897167a4268da50209cef13edc5eedde5
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 01:32:40 +0000

    [automatic commit] 2025-06-20 01:32:40 UTC 203.219.193.98

--- part1/query.sql
+++ part1/query.sql
@@ -1,4 +1,4 @@
-/*     Five users:
+/*
     Five dogs:
         A dog named Max, who is medium-sized and owned by alice123.
         A dog named Bella, who is small and owned by carol123.
@@ -6,12 +6,13 @@
     Five walk requests:
         A request for Max at 2025-06-10 08:00:00 for 30 minutes at Parklands, with status open.
         A request for Bella at 2025-06-10 09:30:00 for 45 minutes at Beachside Ave, with status accepted.
-        Three more walk requests with details of your choosing. */
+        Three more walk requests with details of your choosing.
+*/
 
 INSERT INTO Users(username, email, password_hash, role) VALUES
-('alice123', 'alice@example.com', 'hashed123', 'owner'),
-('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('alice123', 'alice@example.com', 'hashed123', 'owner'), -- 1
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'), -- 2
+('carol123', 'carol@example.com', 'hashed789', 'owner'), -- 3
 ('james123', 'james@example.com', 'hashed101', 'owner'),
 ('johnwalker', 'john@example.com', 'hashed112', 'walker');
 

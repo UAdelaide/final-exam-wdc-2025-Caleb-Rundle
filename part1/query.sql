03ba7dcb3d905ea52ced34cb38a9bcff8d7a69f8
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 01:26:59 +0000

    [automatic commit] 2025-06-20 01:26:59 UTC 203.219.193.98

--- part1/query.sql
+++ part1/query.sql
@@ -13,3 +13,5 @@
         Three more walk requests with details of your choosing. */
 
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owwner');
+
+SELECT * FROM Users;

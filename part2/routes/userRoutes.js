04a0eb84b0ac4a4926f3e18d480c464b9fbf369f
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 22:54:53 +0000

    [automatic commit] 2025-06-20 22:54:53 UTC 203.219.193.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -60,7 +60,7 @@ router.post('/login', async (req, res) => {
     }
 
     req.session.user = rows[0];
-    
+    return res.status(200).send()
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }

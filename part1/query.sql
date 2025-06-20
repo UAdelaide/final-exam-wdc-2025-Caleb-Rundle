f3470baa4c73ae796ed1029c9fb4c4b538e2a3b1
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 01:44:23 +0000

    [automatic commit] 2025-06-20 01:44:23 UTC 203.219.193.98

--- part1/query.sql
+++ part1/query.sql
@@ -20,6 +20,7 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username='james123'), 'John Lick', 'large');
 
 -- You should NOT query by dog name here because dog names are not UNIQUE like with usernames
+-- I'll hardcode 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
 ()
 

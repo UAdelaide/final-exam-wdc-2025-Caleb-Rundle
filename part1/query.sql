f4004fc6251f495b767d8c9f402b18d4ac7bdb73
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 01:38:22 +0000

    [automatic commit] 2025-06-20 01:38:22 UTC 203.219.193.98

--- part1/query.sql
+++ part1/query.sql
@@ -19,8 +19,8 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
-((SELECT user_id FROM Users WHERE username='alice123'), 'Max-2', 'small'),
-((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
+((SELECT user_id FROM Users WHERE username='alice123'), 'Max Junior', 'small'),
+((SELECT user_id FROM Users WHERE username='alice123'), 'Max Senior', 'small'),
 ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small');
 
 SELECT * FROM Users;

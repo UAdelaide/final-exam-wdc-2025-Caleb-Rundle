b801c5f45937ba7f479afe5f427d43f3a1ff64be
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 22:00:12 +0000

    [automatic commit] 2025-06-20 22:00:12 UTC 203.219.193.98

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const mysql = require('mysql');
+import { createPool } from 'mysql';
 
 const app = express();
 

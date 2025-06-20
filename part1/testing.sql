67fddaacb7e89a74f6d9e5e4e605417eb4b80714
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 09:34:22 +0000

    [automatic commit] 2025-06-20 09:34:22 UTC 203.219.193.98

--- part1/testing.sql
+++ part1/testing.sql
@@ -21,7 +21,7 @@ COUNT(WalkRatings.rating_id) AS total_ratings,
 AVG(WalkRatings.rating) AS average_rating,
 COUNT(WalkRequests.request_id) AS completed_walks
 FROM Users
-INNER JOIN WalkRatings ON Users.user_id=WalkRatings.walker_id
+LEFT JOIN WalkRatings ON Users.user_id=WalkRatings.walker_id
 LEFT JOIN WalkApplications ON WalkApplications.walker_id=Users.user_id
 LEFT JOIN WalkRequests ON WalkRequests.request_id=WalkApplications.request_id
 AND WalkRequests.status='completed'

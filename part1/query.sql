33329d3cca3a17e53f1ddcc69412c2ba7811a8f9
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 01:52:25 +0000

    [automatic commit] 2025-06-20 01:52:25 UTC 203.219.193.98

--- part1/query.sql
+++ part1/query.sql
@@ -25,9 +25,9 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
 -- Subquery Method
 ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'open'),
+((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 ((SELECT dog_id FROM Dogs WHERE name='John Lick'), '2025-06-10 00:00:00', 30, 'Shadowlands', 'open'),
-((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name='John Lick'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name='Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open');
 -- Hardcoded (as dog names are not unique)
 

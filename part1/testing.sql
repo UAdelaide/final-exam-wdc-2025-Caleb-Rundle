4a6c63b128d27f09f11e475bb53c890a53e514d6
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 08:52:50 +0000

    [automatic commit] 2025-06-20 08:52:50 UTC 203.219.193.98

--- part1/testing.sql
+++ part1/testing.sql
@@ -26,7 +26,7 @@ GROUP BY WalkRatings.walker_id, Users.username;
 
 SELECT Users.username, COUNT(WalkRequests.request_id) FROM Users
 LEFT JOIN WalkApplications ON WalkApplications.walker_id=Users.user_id
-LEFT JOIN WalkRequests ON WalkRequests.request_id=WalkApplications.request_id
-WHERE Users.role='walker' AND WalkRequests.status='completed'
+LEFT JOIN WalkRequests ON WalkRequests.request_id=WalkApplications.request_id AND WalkRequests.status='completed'
+WHERE Users.role='walker'
 GROUP BY WalkApplications.walker_id, Users.username;
 

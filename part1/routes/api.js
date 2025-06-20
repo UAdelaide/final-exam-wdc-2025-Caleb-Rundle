68227305f9c47f0b3a3d55b71f794243babaea15
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-20 02:44:19 +0000

    [automatic commit] 2025-06-20 02:44:19 UTC 203.219.193.98

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -7,8 +7,9 @@ console.log('Loaded routes/api.js');
 // INNER JOIN Users ON Dogs.owner_id=Users.user_id;
 
 router.get('/dogs', (req, res) => {
-  const query =
-    'SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.owner_id=USERS.user_id';
+  const query = 
+    'SELECT Dogs.name, Dogs.size, Users.username FROM Dogs \
+    INNER JOIN Users ON Dogs.owner_id=USERS.user_id';
   res.status(200).send('test');
 });
 

effc89456bc943de3bfef6d5fce89fc2e2bd55d7
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-21 00:21:59 +0000

    [automatic commit] 2025-06-21 00:21:59 UTC 203.219.193.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -85,7 +85,7 @@ router.get('/dogs', async (req, res) => {
   const [rows] = await db.query(
     `
     SELECT dog_id, name FROM Dogs
-    WHERE owner_id=?
+    WHERE owner_id = ?
     `,
     [req.session.user_id]
   );

3a7e0b9e25653d5e25f7d09a2994692893970a3f
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-21 02:47:46 +0000

    [automatic commit] 2025-06-21 02:47:46 UTC 203.219.193.98

---
+++ part2/routes/extraRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/db');
+
+app.use('/dogs', async (res, req) => {
+  const [rows] = await db.query('SELECT * FROM Dogs');
+  return res.send(rows);
+});
+
+module.exports = router;

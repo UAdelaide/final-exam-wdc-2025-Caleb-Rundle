916f4b8345ce6ae571108d33ccf1e061ced0d650
Caleb-Rundle <caleb.rundle@student.adelaide.edu.au>
2025-06-21 03:43:42 +0000

    [automatic commit] 2025-06-21 03:43:42 UTC 203.219.193.98

--- part2/routes/extraRoutes.js
+++ part2/routes/extraRoutes.js
@@ -5,15 +5,14 @@ const db = require('../models/db');
 router.get('/dogs', async (req, res) => {
   const [rows] = await db.query('SELECT * FROM Dogs');
   const l = rows.length();
-    
+  const { message, status } = await fetch(
+    `https://dog.ceo/api/breeds/image/random/${l}`
+  ).then((response) => response.json());
   return res.send(rows);
 });
 
 router.get('/dogImages/:quantity', async (req, res) => {
   const quantity = req.params.quantity;
-  const { message, status } = await fetch(
-    `https://dog.ceo/api/breeds/image/random/${quantity}`
-  ).then((response) => response.json());
   if (status === 'success') {
     return res.status(200).send(message);
   }
